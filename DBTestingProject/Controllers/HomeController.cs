832f8eb3a473d3adbeabf5a7733edc3b28910a84
Alex <a1980dominov@gmail.com>
2025-04-24 22:56:17 +0300

    REFACTORING MY CODE

--- DBTestingProject/Controllers/HomeController.cs
+++ DBTestingProject/Controllers/HomeController.cs
@@ -8,16 +8,19 @@ namespace DBTestingProject.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         FurnitureDBContext DBContext;
+        FurnitureWithOperation FurnitureWithOperation = new FurnitureWithOperation();
         public HomeController(ILogger<HomeController> logger, FurnitureDBContext furnitureDBContext)
         {
             _logger = logger;
             DBContext = furnitureDBContext;
         }
-        
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            var furniture = DBContext.Furnitures.ToList();
-            return View(furniture);
+            //var furniture = await Task.Run(()=> DBContext.Furnitures.ToList());
+            List<Furniture> myList = await Task.Run(() =>FurnitureWithOperation.GetFurniture(DBContext).ToList());
+            //FurnitureWithOperation.AddFurniture(DBContext);
+            return  View(myList);
         }
 
         public IActionResult Privacy()
---
+++ DBTestingProject/Models/FurnitureWithOperation.cs
@@ -0,0 +1,23 @@
+﻿namespace DBTestingProject.Models
+{
+    public class FurnitureWithOperation
+    {
+        public IEnumerable<Furniture> GetFurniture(FurnitureDBContext dBContext)
+        {
+           List <Furniture> it = dBContext.Furnitures.ToList();
+            return it;
+        }
+        public void AddFurniture(FurnitureDBContext furnitureDB)
+        {
+            furnitureDB.Add(new Furniture
+            {
+                TypeFurniture="Sofa",
+                OutsideView="Soft",
+                Material="Skin",
+                Price=456.89
+
+            });
+            furnitureDB.SaveChanges();
+        }
+    }
+}
