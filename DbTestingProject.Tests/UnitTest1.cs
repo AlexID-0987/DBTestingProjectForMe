b94c63c1fd45ed730bfe10e8f2e7c53093801b8a
Alex <a1980dominov@gmail.com>
2025-06-04 22:10:28 +0300

    I add few tests. My func is changet Edit and Delete items.

--- DbTestingProject.Tests/UnitTest1.cs
+++ DbTestingProject.Tests/UnitTest1.cs
@@ -15,6 +15,8 @@ namespace DbTestingProject.Tests
         
         AddToListFurniture addToList=new AddToListFurniture();
         GetFurnitureMyList getFurniture=new GetFurnitureMyList();
+        IDeleteFurniture deleteFurniture= new DeleteFurniture();
+        IEditFurniture editFurniture = new EditMyListFurniture();
 
         [Test]
         public void AddToFurnitureMyList_Test() 
@@ -54,6 +56,54 @@ namespace DbTestingProject.Tests
                 Assert.AreEqual(2, context.Furnitures.Count());
                 var MyList = getFurniture.GetMyFurniture(context);
                 Assert.IsNotNull(MyList);
+            }
+        }
+        [Test]
+
+        public void Delete_To_Item_MyList_Test()
+        {
+            var option = new DbContextOptionsBuilder<FurnitureDBContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+            using (var context = new FurnitureDBContext(option))
+            {
+                context.Furnitures.Add(new Furniture { Id = 1, TypeFurniture = "Chair11", OutsideView = "Red", Material = "Pe", Price = 32.56 });
+                context.SaveChanges();
+                
+            }
+            using (var context = new FurnitureDBContext(option))
+            {
+
+                deleteFurniture.WithDeleteFurniture(1, context);
+                Assert.IsEmpty(context.Furnitures);
+                Assert.AreNotEqual(1, context.Furnitures.Count());
+            }
+        }
+        [Test]
+        public void Edit_To_MyList_Test()
+        {
+            var option = new DbContextOptionsBuilder<FurnitureDBContext>()
+               .UseInMemoryDatabase(databaseName: "TestDb3")
+               .Options;
+            using (var context = new FurnitureDBContext(option))
+            {
+                context.Furnitures.Add(new Furniture { Id = 1, TypeFurniture = "Chair11", OutsideView = "Red", Material = "Pe", Price = 32.56 });
+                context.SaveChanges();
+
+            }
+            using (var context = new FurnitureDBContext(option))
+            {
+                Furniture furniture = new Furniture { Id = 1, TypeFurniture = "Sofa", OutsideView = "Red", Material = "Pe", Price = 32.56 };
+                editFurniture.EditMyList(furniture, context);
+                context.SaveChanges();
+                var MyList1 = getFurniture.GetMyFurniture(context);
+                foreach (var it in MyList1)
+                {
+                    Assert.AreEqual(it.TypeFurniture, furniture.TypeFurniture);
+                }
+                
+
+
             }
         }
         
